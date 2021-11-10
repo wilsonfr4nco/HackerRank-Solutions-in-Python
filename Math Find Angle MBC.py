194b018ab324f7d3cbc0d9a6be010dbe38201192
Wilson Franco <74009299+wilsonfr4nco@users.noreply.github.com>
2021-11-10 13:44:25 -0300

    Update Math Find Angle MBC.py

--- Math Find Angle MBC.py
+++ Math Find Angle MBC.py
@@ -7,4 +7,4 @@ bc = input()
 h = math.sqrt(ab**2 + bc**2)
 h = h / 2.0
 adj = bc / 2.0
-print str(int(round(math.degrees(math.acos(adj/h))))) + "°"
+print str(int(round(math.degrees(math.acos(adj/h))))) + u'\N{DEGREE SIGN}'
